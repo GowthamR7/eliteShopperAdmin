dc21f50c6ff23457b9162b17d531f9bb4d13ad00
Sridhar S <96615098+GowthamR7@users.noreply.github.com>
2024-04-10 11:52:23 +0530

    project initiated

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div style={{fontSize:'24px', fontWeight:'bold',color:'#ff4141'}}>ELITE SHOPPER</div>
+      <img src={''} className='nav-profile' alt="" />
+    </div>
+  )
+}
+
+export default Navbar
